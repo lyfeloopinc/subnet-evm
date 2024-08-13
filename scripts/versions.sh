2a67449e9378c84df3ab584d883fc4d831894ca7
Ian Suvak <ian.suvak@avalabs.org>
2024-08-13 14:28:08 -0400

    bump avalanchego again to bump coreth indirectly

--- scripts/versions.sh
+++ scripts/versions.sh
@@ -4,7 +4,7 @@
 # shellcheck disable=SC2034
 
 # Don't export them as they're used in the context of other calls
-AVALANCHE_VERSION=${AVALANCHE_VERSION:-'c7cf573a95ffc1d54a53807ccf3352f44964ca54'}
+AVALANCHE_VERSION=${AVALANCHE_VERSION:-'b20691fae7b0fe54b536c30c034ab0f4964c647c'}
 GINKGO_VERSION=${GINKGO_VERSION:-'v2.2.0'}
 
 # This won't be used, but it's here to make code syncs easier
